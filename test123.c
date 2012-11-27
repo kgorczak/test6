1e504c7c60d595ce84782fc30794b3a7e61dcda1
Krzysztof Górczak <kgorczak@p109-7.labpk.inf.ug.edu.pl>
2012-11-27 09:58:58 +0100

    GUFNO

--- test123.c
+++ test123.c
@@ -5,18 +5,14 @@ int main()
 
 printf("Hahaha");
 printf("Siema");
-<<<<<<< HEAD
 printf("Hahaha");
 printf("Siema");
-=======
-
 printf("Siema");
 printf("Siema123");
->>>>>>> c2ca72320fd05dd590bc6fe9317dca6ef3a41c3d
 printf("Siema");
 printf("Siema");
 printf("Siema");
-printf("Siema");
+printf("gufno");
 printf("Siema");
 printf("Siema");
 printf("Siema");
